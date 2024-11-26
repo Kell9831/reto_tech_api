d56c98870987986e420781ac8d0203029ddef1c3
kellkyrc <kellycarrion@gmail.com>
2024-11-25 22:26:13 -0500

    update

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,12 +4,14 @@ import { AuthService } from './auth.service';
 import { UsersModule } from 'src/users/users.module';
 import { AuthController } from './auth.controller';
 import { JwtStrategy } from './strategies/jwt.strategy';
+import * as dotenv from 'dotenv';
 
+dotenv.config();
 @Module({
     imports: [
         UsersModule, 
         JwtModule.register({
-          secret: 'secret', 
+          secret: process.env.JWT_SECRET, 
           signOptions: { expiresIn: '1h' }, 
         }),
       ],
